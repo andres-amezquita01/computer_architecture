33dfebb7e8ffd8c02bb86b13e02d4d20b0eac98c
andres-amezquita01 <andresfelipe.amezquita01@gmail.com>
2023-05-02 17:39:00 -0500

    Add math file

--- math.asm
+++ math.asm
@@ -1,97 +1,89 @@
-
-
-;
-; file: math.asm
-; This program demonstrates how the integer multiplication and division
-; instructions work.
-;
-; To create executable:
-; nasm -f coff math.asm
-; gcc -o math math.o driver.c asm_io.o
-
 %include "asm_io.inc"
 
-segment .data
-;
-; Output strings
-;
-prompt          db    "Enter a number: ", 0
-square_msg      db    "Square of input is ", 0
-cube_msg        db    "Cube of input is ", 0
-cube25_msg      db    "Cube of input times 25 is ", 0
-quot_msg        db    "Quotient of cube/100 is ", 0
-rem_msg         db    "Remainder of cube/100 is ", 0
-neg_msg         db    "The negation of the remainder is ", 0
+%macro PRINT_STRING 1
+pusha
+    mov eax, %1
+    call print_string
+    popa
+%endmacro
+
+%macro PRINT_INT 1
+    pusha
+    mov eax, %1
+    call print_int
+    popa
+%endmacro
+
+%macro PRINT_NL 0
+    pusha
+    call print_nl
+    popa
+%endmacro
+
+
+segment .data ; Output strings
+        prompt db "Enter a number: ", 0
+        square_msg db "Square of input is ", 0
+        cube_msg db "Cube of input is ", 0
+        cube25_msg db "Cube of input times 25 is ", 0
+        quot_msg db "Quotient of cube/100 is ", 0
+        rem_msg db "Remainder of cube/100 is ", 0
+        neg_msg db "The negation of the remainder is ", 0
 
 segment .bss
-input   resd 1
-
+    input resd 1
 
 segment .text
-        global  asm_main
+    global asm_main
+
+
+
 asm_main:
-        enter   0,0               ; setup routine
-        pusha
+    enter 0,0 ; setup routine
+    pusha
 
-        mov     eax, prompt
-        call    print_string
+    PRINT_STRING prompt
+    call read_int
+    mov [input], eax
 
-        call    read_int
-        mov     [input], eax
+    imul eax ; edx:eax = eax * eax
+    mov ebx, eax ; save answer in ebx
 
-        imul    eax               ; edx:eax = eax * eax
-        mov     ebx, eax          ; save answer in ebx
-        mov     eax, square_msg
-        call    print_string
-        mov     eax, ebx
-        call    print_int
-        call    print_nl
+    PRINT_STRING square_msg
+    PRINT_INT ebx
+    PRINT_NL
 
-        mov     ebx, eax
-        imul    ebx, [input]      ; ebx *= [input]
-        mov     eax, cube_msg
-        call    print_string
-        mov     eax, ebx
-        call    print_int
-        call    print_nl
+    imul ebx,[input]; ebx*=[input]
+    PRINT_STRING cube_msg
+    PRINT_INT ebx
+    PRINT_NL
 
-        imul    ecx, ebx, 25      ; ecx = ebx*25
-        mov     eax, cube25_msg
-        call    print_string
-        mov     eax, ecx
-        call    print_int
-        call    print_nl
+    imul ecx, ebx, 25 ; ecx = ebx*25
+    PRINT_STRING cube25_msg
+    PRINT_INT ecx
+    PRINT_NL
 
-        mov     eax, ebx
-        cdq                       ; initialize edx by sign extension
-        mov     ecx, 100          ; can't divide by immediate value
-        idiv    ecx               ; edx:eax / ecx
-        mov     ecx, eax          ; save quotient into ecx
-        mov     eax, quot_msg
-        call    print_string
-        mov     eax, ecx
-        call    print_int
-        call    print_nl
-        mov     eax, rem_msg
-        call    print_string
-        mov     eax, edx
-        call    print_int
-        call    print_nl
-        
-        neg     edx               ; negate the remainder
-        mov     eax, neg_msg
-        call    print_string
-        mov     eax, edx
-        call    print_int
-        call    print_nl
+    mov eax, ebx
+    cdq ; initialize edx by sign extension
+    mov ecx, 100 ; can’t divide by immediate value
+    idiv ecx ; edx:eax / ecx
 
-        popa
-        mov     eax, 0            ; return back to C
-        leave                     
-        ret
+    mov ecx, eax ; save quotient into ecx
+    PRINT_STRING quot_msg
+    PRINT_INT ecx
+    PRINT_NL
 
+    PRINT_STRING rem_msg
+    PRINT_INT edx
+    PRINT_NL
 
+    neg edx ; negate the remainder
+    PRINT_STRING neg_msg
+    PRINT_INT edx
+    PRINT_NL
 
-
-
+    popa
+    mov eax, 0 ; return back to C
+    leave
+    ret
 
